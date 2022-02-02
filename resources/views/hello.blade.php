bcc4adfe07f7ef83ff21016dcc7f4e7cbbe43a91
kotomi1338 <kotomin1216@gmail.com>
2022-02-02 20:10:29 +0900

    Hello World!!

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Puti Hackason</title>
+</head>
+
+<body>
+  <h2>Hello World Laravel!!</h2>
+  <p>ぷちハッカソン!</p>
+</body>
+
+</html>
